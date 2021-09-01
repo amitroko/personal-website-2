6109eda2ef83986e8cb73a891c33847327ed6bf8
Alec Mitrokostas <apmitrokostas@gmail.com>
2021-09-01 11:59:58 -0400

    new links

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ function App() {
         <img src={Headshot} alt="Photo of Alec Mitrokostas"/>
         <h1>Alec Mitrokostas</h1>
         <p style={{color: "whitesmoke"}}>Currently:<br/>
-        SWE Intern @ MCA Connect | Denver, CO<br/>
-        4th-Year Computer Science and Business Student @ Lehigh University | Bethlehem, PA<br/>
-        Scalable Systems Software Research Group @ Lehigh University | Bethlehem, PA</p>
+        4th-Year <a className="text-link" href="https://csb.lehigh.edu/" target="_blank" rel="noopener noreferrer">Computer Science and Business</a> Student @ Lehigh University | Bethlehem, PA<br/>
+        <a className="text-link" href="https://wordpress.lehigh.edu/sss/" target="_blank" rel="noopener noreferrer">Scalable Systems Software Research Group</a> @ Lehigh University | Bethlehem, PA</p>
+        <p style={{color: "whitesmoke"}}>Previously:<br/>
+        SWE Intern @ <a className="text-link" href="https://mcaconnect.com/" target="_blank" rel="noopener noreferrer">MCA Connect</a> | Denver, CO<br/>
+        Secretary and Professional Development Chairman, Comp. Sci. and Business Assn. @ Lehigh University | Bethlehem, PA</p>
         <div className="links">
           <a className="icon" href="mailto:apm322@lehigh.edu" target="_blank" rel="noopener noreferrer">
             <Mail height="48px" fill="whitesmoke"/>
